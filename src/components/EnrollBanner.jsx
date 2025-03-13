acd04ef497d819eeb233948bbefa62ebd206c95c
aaron <aaronmat.global@gmail.com>
2025-03-13 18:17:19 +0530

    added google map fix

--- src/components/EnrollBanner.jsx
+++ src/components/EnrollBanner.jsx
@@ -1,7 +1,7 @@
 const EnrollBanner = () => {
     return (
         <section className="enroll-banner">
-            <h1>Check Out Our Gallary <br /> To know more</h1>
+            <h1>Check Out Our Gallery <br /> To know more</h1>
             <a href="/Gallery" className="hero-btn">Click</a>
         </section>
     );
